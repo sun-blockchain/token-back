99c9e89652e70aaa803680da14290156b806b051
trinhtan <lop12a1trinhvantan@gmail.com>
2020-09-01 15:29:58 +0700

    add Point smart contract

---
+++ contract/contracts/Point.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.5.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
+
+/**
+ * @title Point
+ **/
+
+contract Point is ERC20Mintable, ERC20Burnable, ERC20Detailed {
+    constructor(string memory _symbol)
+        public
+        ERC20Detailed(_symbol, _symbol, 18)
+    {
+        // Intentionally left blank
+    }
+}
