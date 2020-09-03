973089cb4f09cf199a0a865b3e9d34a441b31905
nghianv-1868 <ngo.van.nghia@sun-asterisk.com>
2020-09-03 17:22:54 +0700

    add-market

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,27 +1,28 @@
-import Vue from 'vue'
-import VueRouter, { RouteConfig } from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from 'vue';
+import VueRouter, { RouteConfig } from 'vue-router';
+import Home from '../views/Home.vue';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
-  const routes: Array<RouteConfig> = [
+const routes: Array<RouteConfig> = [
   {
     path: '/',
     name: 'Home',
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/markets',
+    name: 'Markets',
     // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
+    // this generates a separate chunk (Markets.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "Markets" */ '../views/Markets.vue')
   }
-]
+];
 
 const router = new VueRouter({
+  mode: 'history',
   routes
-})
+});
 
-export default router
+export default router;
